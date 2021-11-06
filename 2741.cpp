f85da0510b02d51b2373ef85ee0b4e1e648ae506
amylo <amylose1110@gmail.com>
2021-11-06 22:23:59 +0900

    2 solve

---
+++ 2741.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+
+    for (int j = 1; j <=i; j++)
+    {
+        printf("%d\n",j);
+    }
+}
---
+++ 2742.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+
+    for (int j = i; j >0; j--)
+    {
+        printf("%d\n",j);
+    }
+}
