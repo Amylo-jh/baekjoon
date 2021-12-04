6fe062a05a7225fb2f6b0f1d18d4163b1c4d9f2c
Amylo-jh <amylose1110@gmail.com>
2021-12-04 19:54:44 +0900

    1 solve

---
+++ 4562.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        x<y?cout<<"NO BRAINS"<<endl:cout<<"MMM BRAINS"<<endl;
+    }
+}
