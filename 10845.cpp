5769e2220d2d383c172acb4545d1faab46f372c2
Amylo-jh <amylose1110@gmail.com>
2021-12-12 02:01:30 +0900

    1 solve

---
+++ 10845.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int commands;
+    queue <int> num_queue {};
+    string command;
+    int temp;
+
+    cin >> commands;
+
+    for(int i = 0; i < commands; i++)
+    {
+        cin >> command;
+
+        if(command == "push")
+        {
+            cin >> temp;
+            num_queue.push(temp);
+        }
+        else if(command == "pop")
+        {
+            if(num_queue.size() == 0)
+            {
+                cout << -1 << endl;
+                continue;
+            }
+            temp = num_queue.front();
+            cout << temp << endl;
+            num_queue.pop();
+        }
+        else if(command == "size")
+        {
+            cout << num_queue.size() << endl;
+        }
+        else if(command == "empty")
+        {
+            if(num_queue.size() == 0)
+            {
+                cout << 1 << endl;
+            }
+            else
+            {
+                cout << 0 << endl;
+            }
+        }
+        else if(command == "front")
+        {
+            if(num_queue.size() == 0)
+            {
+                cout << -1 << endl;
+                continue;
+            }
+            cout << num_queue.front() << endl;
+        }
+        else if(command == "back")
+        {
+            if(num_queue.size() == 0)
+            {
+                cout << -1 << endl;
+                continue;
+            }
+            cout << num_queue.back() << endl;
+        }
+    }
+}
