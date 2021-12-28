807c4f0d0421c39116c23984ae941ef4383d494e
Amylo-jh <amylose1110@gmail.com>
2021-12-28 12:09:44 +0900

    1 solve

---
+++ 15963.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long require;
+    long long provide;
+
+    cin >> require >> provide;
+
+    cout << (provide >= require) ;
+}
