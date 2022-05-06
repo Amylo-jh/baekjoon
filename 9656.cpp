acf353a70d7f8e0a31c910632b26f8db0af01b7a
Amylo-jh <amylose1110@gmail.com>
2022-05-06 22:27:32 +0900

    1 solve

---
+++ 9656.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n % 2)
+    {
+        cout << "CYK";
+    }
+    else
+    {
+        cout << "SK";
+    }
+}
+
+/*
+돌이 1개일때
+1
+SK
+
+2
+1 1
+CY
+
+3
+1 1 1
+3
+SK
+
+4
+1 3
+3 1
+CY
+
+5
+1 1 3
+1 3 1
+3 1 1
+SK
+
+6
+
+1 1 1 3
+1 1 3 1
+1 3 1 1
+3 1 1 1
+
+
+7
+
+*/
