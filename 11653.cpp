83066f48761ab875a3ce49aa0bb5e45dc468d8a9
Amylo-jh <amylose1110@gmail.com>
2022-05-14 17:44:41 +0900

    1 solve

---
+++ 11653.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 2;
+    while(i <= n)
+    {
+        if(n % i == 0)
+        {
+            cout << i << endl;
+            n = n / i;
+            i = 2;
+            continue;;
+        }
+        i++;
+    }
+}
