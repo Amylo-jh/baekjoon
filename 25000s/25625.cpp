60087b9b45231f920825a651c7f040dc52b169e3
Amylo-jh <amylose1110@gmail.com>
2023-06-13 01:21:11 +0900

    1 solve

---
+++ 25000s/25625.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if(x < y)
+    {
+        cout << y - x;
+    }
+    else
+    {
+        cout << x + y;
+    }
+}
