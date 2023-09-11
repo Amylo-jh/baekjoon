5c838c13a01ee2420a5cf5aff845b5e189220469
Amylo <amylose1110@gmail.com>
2023-09-12 02:29:34 +0900

    1 solve

---
+++ 25000s/25083.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str = 
+        "         ,r'\"7\n"
+        "r`-_   ,'  ,/\n"
+        " \\. \". L_r'\n"
+        "   `~\\/\n"
+        "      |\n"
+        "      |";
+
+    cout << str;
+}
