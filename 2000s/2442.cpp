3c0a732c5c7538a38d6421246772e9443f4620e4
Amylo <amylose1110@gmail.com>
2024-01-09 21:57:10 +0900

    1 solve

---
+++ 2000s/2442.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = n; i > 0; i--)
+    {
+        for(int j = 1; j < i; j++)
+        {
+            cout << " ";
+        }
+        for(int j = i; j <= n; j++)
+        {
+            cout << "*";
+        }
+        for(int j = i; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
