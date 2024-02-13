50b7d12052fb73e321738a6c4ccf6f0b077f5374
Amylo <amylose1110@gmail.com>
2024-02-14 01:35:56 +0900

    1 solve

---
+++ 11000s/11134.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a / b + !!(a%b) << "\n";
+    }
+}
