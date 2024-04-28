eabedc7f57bffc1cbcc1132765a6d253a3b9d79c
Amylo <amylose1110@gmail.com>
2024-04-29 02:47:56 +0900

    1 solve

---
+++ 1000s/1069.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x, y, d, t;
+    cin >> x >> y >> d >> t;
+
+    double distance = sqrt(x * x + y * y);
+    double minTime = distance;
+
+    double RemainingDistance = distance;
+    double currTime = 0;
+    while(RemainingDistance > 0)
+    {
+        if(RemainingDistance < d*2)
+        {
+            minTime = min(minTime, t + t + currTime);
+        }
+
+        RemainingDistance -= d;
+        currTime += t;
+        if(RemainingDistance >= 0)
+        {
+            minTime = min(minTime, RemainingDistance + currTime);
+            
+        }
+    }
+
+    if(RemainingDistance < 0)
+    {
+        minTime = min(minTime, -RemainingDistance + currTime);
+    }
+    // RemainingDistance -= d;
+    // currTime += t;
+    // minTime = min(minTime, -RemainingDistance + currTime);
+    
+    cout.precision(20);
+    cout << minTime;
+}
