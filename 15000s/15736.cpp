c6fba91c1ad51867a07312322d50e8e0f7ead5fc
Amylo <amylose1110@gmail.com>
2024-06-11 00:58:16 +0900

    1 solve

---
+++ 15000s/15736.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    for(int i = 1; i*i <= n; i++)
+    {
+        count++;
+    }
+
+    cout << count << '\n';
+}
