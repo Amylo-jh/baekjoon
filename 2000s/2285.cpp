25f1afa5470efae3dcd99f6e4d0d44e51dd7ada4
Amylo <amylose1110@gmail.com>
2024-07-08 00:24:04 +0900

    1 solve

---
+++ 2000s/2285.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    long long population = 0;
+    vector <pair <int, int>> arr(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i].first >> arr[i].second;
+        population += arr[i].second;
+    }
+    sort(arr.begin(), arr.end());
+
+    long long pop_sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        pop_sum += arr[i].second;
+        if(pop_sum >= (population + 1) / 2)
+        {
+            cout << arr[i].first;
+            break;
+        }
+    }
+}
