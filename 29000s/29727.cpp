3ae2472c22c80a14dc306f30a5dc7de2d98d6197
Amylo <amylose1110@gmail.com>
2024-09-21 03:11:05 +0900

    fix shorter

--- 29000s/29727.cpp
+++ 29000s/29727.cpp
@@ -2,40 +2,36 @@
 
 using namespace std;
 
+long long n, xa, xb, ya, yb, sum = 0;
+
+void normalize()
+{
+    xa = max(-1LL, xa);
+    xa = min(n, xa);
+    xb = max(-1LL, xb);
+    xb = min(n, xb);
+    ya = max(-1LL, ya);
+    ya = min(n, ya);
+    yb = max(-1LL, yb);
+    yb = min(n, yb);
+
+    return;
+}
+
 int main()
 {
-    long long n, xa, xb, ya, yb;
     cin >> n >> xa >> ya >> xb >> yb;
-
-    long long sum = 0;
     sum = (n+1)*n/2 * (n+1)*n/2;
 
     if(xa == xb)
     {
-        xa = max(-1LL, xa);
-        xa = min(n, xa);
-        xb = max(-1LL, xb);
-        xb = min(n, xb);
-        ya = max(-1LL, ya);
-        ya = min(n, ya);
-        yb = max(-1LL, yb);
-        yb = min(n, yb);
-
+        normalize();
         long long diff = abs(ya - yb);
         sum += diff*(diff-1)/2 * (n+1);
     }
-    
     if(ya == yb)
     {
-        xa = max(-1LL, xa);
-        xa = min(n, xa);
-        xb = max(-1LL, xb);
-        xb = min(n, xb);
-        ya = max(-1LL, ya);
-        ya = min(n, ya);
-        yb = max(-1LL, yb);
-        yb = min(n, yb);
-
+        normalize();
         long long diff = abs(xa - xb);
         sum += diff*(diff-1)/2 * (n+1);
     }
