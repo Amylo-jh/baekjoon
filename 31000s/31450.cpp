abaa76e09717f3600b3526718b086a279d505594
Amylo <amylose1110@gmail.com>
2025-01-31 00:42:46 +0900

    1 solve

---
+++ 31000s/31450.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+
+    if(a % b)
+    {
+        cout << "No";
+    }
+    else
+    {
+        cout << "Yes";
+    }
+}
