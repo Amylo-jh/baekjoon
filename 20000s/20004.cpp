6d12ced11d49154152a3ef26c74835656a217fec
Amylo-jh <amylose1110@gmail.com>
2025-04-08 23:41:58 +0900

    1 solve

---
+++ 20000s/20004.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(30 % (i+1) == 0)
+        {
+            cout << i << "\n";
+        }
+    }
+}
