7ccdbd145626016bdc1da3b30b0861e4f13b70dc
Amylo-jh <amylose1110@gmail.com>
2025-05-22 01:16:56 +0900

    1 solve

---
+++ 13000s/13322.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        cout << i << "\n";
+    }
+}
