92e7cfe58b9fa568a48203bf65e005ec1dd1e6c8
Amylo-jh <amylose1110@gmail.com>
2025-07-27 22:35:39 +0900

    1 solve

---
+++ Temp_Contest/1528-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        string str;
+        cin >> str;
+        cout << str << "DORO ";
+    }
+}
