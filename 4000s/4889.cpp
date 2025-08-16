580150ff16bdd2453150fe33ab73e2ba94cc2a5d
Amylo-jh <amylose1110@gmail.com>
2025-08-17 00:33:14 +0900

    1 solve

---
+++ 4000s/4889.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int tc = 1;
+    while(tc)
+    {
+        string str;
+        cin >> str;
+        if(str[0] == '-')
+        {
+            break;
+        }
+
+        int stk = 0;
+        int answer = 0;
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str[i] == '{')
+            {
+                stk++;
+            }
+            else
+            {
+                if(stk == 0)
+                {
+                    answer++;
+                    stk++;
+                }
+                else
+                {
+                    stk--;
+                }
+            }
+        }
+        answer += stk/2;
+
+        cout << tc << ". " << answer << "\n";
+        tc++;
+    }
+}
