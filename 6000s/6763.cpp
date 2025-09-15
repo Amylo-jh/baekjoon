ff5821bd5fef8ded42295791d906a1c400e4b9fa
Amylo-jh <amylose1110@gmail.com>
2025-09-16 02:49:20 +0900

    1 solve

---
+++ 6000s/6763.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if(b - a > 30)
+    {
+        cout << "You are speeding and your fine is $500.";
+    }
+    else if(b - a > 20)
+    {
+        cout << "You are speeding and your fine is $270.";
+    }
+    else if(b - a > 0)
+    {
+        cout << "You are speeding and your fine is $100.";
+    }
+    else
+    {
+        cout << "Congratulations, you are within the speed limit!";
+    }
+}
