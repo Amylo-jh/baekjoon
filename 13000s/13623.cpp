eaaf8d1eb75401f310a95592ab407d7f4ccfb0c4
Amylo-jh <amylose1110@gmail.com>
2025-11-01 21:19:23 +0900

    1 solve

---
+++ 13000s/13623.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a != b && a != c)
+    {
+        cout << "A";
+    }
+    else if(b != a && b != c)
+    {
+        cout << "B";
+    }
+    else if(c != a && c != b)
+    {
+        cout << "C";
+    }
+    else
+    {
+        cout << "*";
+    }
+}
