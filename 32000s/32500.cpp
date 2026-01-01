b30ce94b2465e72fdc8eb95427cec646c25e853c
Amylo-jh <amylose1110@gmail.com>
2026-01-02 03:07:52 +0900

    1 solve

---
+++ 32000s/32500.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, tmp;
+    cin >> n;
+    vector <int> arr(51);
+    vector <int> answer;
+    for(int i = 0; i < n*50; i++)
+    {
+        cin >> tmp;
+        arr[tmp]++;
+    }
+    for(int i = 1; i <= 50; i++)
+    {
+        if(arr[i] > 2*n)
+        {
+            answer.push_back(i);
+        }
+    }
+    for(auto i : answer)
+    {
+        cout << i << " ";
+    }
+}
