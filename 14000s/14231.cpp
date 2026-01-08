6dcac133f36afedbe2770ba80ed397729f335b81
Amylo-jh <amylose1110@gmail.com>
2026-01-09 03:41:38 +0900

    1 solve

---
+++ 14000s/14231.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    vector <int> lis;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    lis.push_back(arr[0]);
+
+    int lis_size = 0;
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > lis[lis_size])
+        {
+            lis.push_back(arr[i]);
+            lis_size++;
+        }
+        else
+        {
+            int pos = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+            lis[pos] = arr[i];
+        }
+    }
+    cout << lis.size();
+}
