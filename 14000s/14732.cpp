6f8f4e9a3688e5bc6878e52e7525ba229e84223d
Amylo-jh <amylose1110@gmail.com>
2026-03-07 03:12:26 +0900

    1 solve

--- 14000s/14732.cpp
+++ 14000s/14732.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    int n;
+    vector <vector <bool>> arr(501, vector <bool>(501));
+    int n, x1, y1, x2, y2;
     cin >> n;
-
-    int sum = 2;
-    while(sum-1 < n)
+    for(int i = 0; i < n; i++)
     {
-        n -= sum-1;
-        sum++;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for(int x = x1; x < x2; x++)
+        {
+            for(int y = y1; y < y2; y++)
+            {
+                arr[x][y] = true;
+            }
+        }
     }
-
-    cout << sum-n << " " << n;
+    int count = 0;
+    for(int i = 0; i <= 500; i++)
+    {
+        for(int j = 0; j <= 500; j++)
+        {
+            if(arr[i][j])
+            {
+                count++;
+            }
+        }
+    }
+    cout << count;
 }
